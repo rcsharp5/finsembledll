375357c4273e1a70d7f0db58a1d11c738c1b2d95
NinjaManatee <andy@chartiq.com>
2018-12-04 14:58:16 -0500

    Fix whitespace

--- WPFExample/App.xaml.cs
+++ WPFExample/App.xaml.cs
@@ -9,25 +9,25 @@ namespace WPFExample
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application
-    {
+	{
 		/// <summary>
 		/// The logger
 		/// </summary>
 		private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
 		protected override void OnStartup(StartupEventArgs e)
-        {
+		{
 			Logger.Debug("OnStartup");
 
-            Debugger.Launch(); // uncomment to launch debugger when executing externally.
-            var mainWindow = new MainWindow(e.Args); // send command line arguments to main window.
-        }
+			Debugger.Launch(); // uncomment to launch debugger when executing externally.
+			var mainWindow = new MainWindow(e.Args); // send command line arguments to main window.
+		}
 
-        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
 			Logger.Error("An Unhandled Exception has occurred. Please Check your event Logs.", e.Exception);
 			Debugger.Launch();
-            MessageBox.Show("An Unhandled Exception has occurred. Please Check your event Logs.");
-        }
-    }
+			MessageBox.Show("An Unhandled Exception has occurred. Please Check your event Logs.");
+		}
+	}
 }
