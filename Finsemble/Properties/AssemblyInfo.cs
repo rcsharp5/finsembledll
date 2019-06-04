1ef3d1bf6d19ca6d5ace2dfac2265faeca7b5881
Siddharth Dalal <sgd2z@users.noreply.github.com>
2019-06-04 16:21:01 -0400

    Release 3.9.0
    Release 3.9.0

--- Finsemble/Properties/AssemblyInfo.cs
+++ Finsemble/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FinsembleBridge")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Finsemble DLL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FinsembleBridge")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("ChartIQ Inc")]
+[assembly: AssemblyProduct("FinsembleDLL")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+[![Build Status](https://dev.azure.com/chartiq/Finsemble-dll/_apis/build/status/ChartIQ.finsemble-dll?branchName=master)](https://dev.azure.com/chartiq/Finsemble-dll/_build/latest?definitionId=3&branchName=master)
+
+# CI/CD system
+## Master Branch
+No tests are run, system only ensures that a build is made.
+
+Finsemble.dll will be packaged up into a nuget package and published to https://www.nuget.org/packages/Finsemble/ with the version number that is set in AssembleInfo.cs.
+
+Note: If an existing version is already on nuget (even in an unpublished or pending state) then the publish process will fail. This is expected behavoir. Be sure to bump the version number accordingly prior to merging to master.
+
+## PR
+As of now the system only checks that a build is made. There are no tests to run. No assets are published.
+
+# Finsemble DLL
+The project `Finsemble` will build Finsemble.dll
+
 # finsemble-wpf
 
 
